d755dcf08211ef166a51b41f05b0bc0bafa4faec
SpaceJamUA <freezo_hpi@mail.ru>
2017-03-24 23:49:40 +0400

    Task(2.1)

--- Test_1/main.js
+++ Test_1/main.js
@@ -1,12 +1,22 @@
+'use strict';
+var CONTENT_NAME = ['TotalCompanies','ListOfCompanies','CompaniesByLocation','News'];
 var userObj = {};
-
+$('#userConditions').on('click', function() {
+  $('.sendFormReg')[0].disabled = !$('.sendFormReg')[0].disabled;
+})
 $('#sendBtn').on('click', function(){
   userObj.name = $('#userName')[0].value;
   userObj.secondname = $('#userSecondName')[0].value;
   userObj.email = $('#userEmail')[0].value;
   userObj.gender = $('#userGender')[0].value.toLowerCase();
   userObj.pass = $('#userPass')[0].value;
-  userObj.condition = $('#userConditions').is(':checked');
+  for(var i = 0;i < $('.groupRegistration').children().length; i++){
+    $('.groupRegistration').children()[i].classList.remove('has-error','has-feedback');
+
+    if($('.groupRegistration').children()[i].classList == 'textError'){
+      $('.groupRegistration').children()[i].remove();
+    }
+  }
   $.ajax({
     url: "http://codeit.pro/frontTestTask/user/registration",
     method: "POST",
@@ -14,12 +24,108 @@ $('#sendBtn').on('click', function(){
     dataType: "html",
     success: function (data) {
       if(JSON.parse(data).status == 'OK'){
-          return 'some Magic';
-      }else if(JSON.parse(data).status == 'Error'){
-        alert(JSON.parse(data).message);
+        console.log(new Date());
+        createDivContent(CONTENT_NAME);
+        renderContent();
       }else{
-        alert(JSON.parse(data).message);
+        return checkFieldError(JSON.parse(data).message);
       }
     }
   });
 })
+
+function checkFieldError(data) {
+  console.log(data);
+  var errorMsg = document.createElement('div');
+  errorMsg.classList = 'textError';
+  errorMsg.textContent = data;
+
+  if(data.toLowerCase().indexOf('secondname')+1){
+
+    $('#userSecondName')[0].parentNode.classList.add('has-error','has-feedback');
+    $('#userSecondName')[0].parentNode.after(errorMsg);
+  }else if(data.toLowerCase().indexOf('name')+1){
+    $('#userName')[0].parentNode.classList.add('has-error','has-feedback');
+    $('#userName')[0].parentNode.after(errorMsg);
+  }else if(data.toLowerCase().indexOf('email')+1){
+    $('#userEmail')[0].parentNode.classList.add('has-error','has-feedback');
+    $('#userEmail')[0].parentNode.after(errorMsg);
+  }else if(data.toLowerCase().indexOf('pass')+1){
+    $('#userPass')[0].parentNode.classList.add('has-error','has-feedback');
+    $('#userPass')[0].parentNode.after(errorMsg);
+  } else if(data.toLowerCase().indexOf('gender')+1){
+    $('#userGender')[0].parentNode.classList.add('has-error','has-feedback');
+    $('#userGender')[0].parentNode.after(errorMsg);
+  }
+}
+
+
+function createDivContent(contentName) {
+  $('body')[0].innerHTML = '';
+  for(var i = 0; i < contentName.length; i++){
+    var mainContent = document.createElement('div');
+    mainContent.classList = ('mainContent');
+
+
+    var tagContent = document.createElement('div');
+    tagContent.classList = ('col-lg-6 col-sm-12  mainClass');
+
+    var downloadContent = document.createElement('div');
+    downloadContent.classList = ('downloadContent');
+
+    var headerOfContent = document.createElement('div');
+    headerOfContent.classList = ('header');
+
+    var headerText = document.createElement('b');
+    headerText.textContent = contentName[i];
+
+
+    // var loadingDiv = document.createElement('div')
+    // loadingDiv.classList = ('loadingSrc');
+    // loadingDiv.id = contentName[i];
+
+    //
+    var contentBody = document.createElement('div')
+    contentBody.classList = ('contentBody');
+    contentBody.id = contentName[i];
+    //
+
+    var loadingImg = document.createElement('img');
+    loadingImg.classList = ('loadingSrc');
+    loadingImg.src = '5.gif';
+    loadingImg.alt = 'Loading...';
+
+    $(headerOfContent).append(headerText);
+    $(downloadContent).append(headerOfContent);
+    $(contentBody).append(loadingImg);
+
+    $(downloadContent).append(contentBody);
+    $(tagContent).append(downloadContent);
+    $(mainContent).append(tagContent);
+    $(document.body).append(mainContent);
+  }
+}
+
+function renderContent() {
+  $.ajax({
+    url: "http://codeit.pro/frontTestTask/company/getList",
+    method: "GET",
+    dataType: "JSON",
+    success: function (data) {
+      console.log(data.list);
+      createTotalContent(data.list);
+    }
+  })
+
+}
+
+function createTotalContent(data){
+  $('#TotalCompanies > img').remove();
+  var circle = document.createElement('div');
+  circle.classList = ('circle');
+  var totalCompany = document.createElement('center');
+  totalCompany.classList = ('totalCompanyNum');
+  totalCompany.textContent = data.length;
+  $(circle).append(totalCompany);
+  $('#TotalCompanies').append(circle);
+}
