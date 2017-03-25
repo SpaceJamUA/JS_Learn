1f59efbf87ebb5e7cc042ccb544c24dc71d786e2
SpaceJamUA <freezo_hpi@mail.ru>
2017-03-25 22:49:56 +0400

    Task(2.1.1 - 2.1.3)

--- Test_1/main.js
+++ Test_1/main.js
@@ -24,7 +24,6 @@ $('#sendBtn').on('click', function(){
     dataType: "html",
     success: function (data) {
       if(JSON.parse(data).status == 'OK'){
-        console.log(new Date());
         createDivContent(CONTENT_NAME);
         renderContent();
       }else{
@@ -35,7 +34,6 @@ $('#sendBtn').on('click', function(){
 })
 
 function checkFieldError(data) {
-  console.log(data);
   var errorMsg = document.createElement('div');
   errorMsg.classList = 'textError';
   errorMsg.textContent = data;
@@ -68,7 +66,7 @@ function createDivContent(contentName) {
 
 
     var tagContent = document.createElement('div');
-    tagContent.classList = ('col-lg-6 col-sm-12  mainClass');
+    tagContent.classList = ('col-lg-6 col-sm-12 col-xs-12 mainClass');
 
     var downloadContent = document.createElement('div');
     downloadContent.classList = ('downloadContent');
@@ -79,16 +77,9 @@ function createDivContent(contentName) {
     var headerText = document.createElement('b');
     headerText.textContent = contentName[i];
 
-
-    // var loadingDiv = document.createElement('div')
-    // loadingDiv.classList = ('loadingSrc');
-    // loadingDiv.id = contentName[i];
-
-    //
     var contentBody = document.createElement('div')
     contentBody.classList = ('contentBody');
     contentBody.id = contentName[i];
-    //
 
     var loadingImg = document.createElement('img');
     loadingImg.classList = ('loadingSrc');
@@ -112,8 +103,10 @@ function renderContent() {
     method: "GET",
     dataType: "JSON",
     success: function (data) {
-      console.log(data.list);
+      // console.log(data.list);
       createTotalContent(data.list);
+      createListOfCompany(data.list);
+      mathLocation(data.list);
     }
   })
 
@@ -128,4 +121,83 @@ function createTotalContent(data){
   totalCompany.textContent = data.length;
   $(circle).append(totalCompany);
   $('#TotalCompanies').append(circle);
+}
+
+function createListOfCompany(data) {
+  var ulCompany = document.createElement('ul');
+  ulCompany.classList = ('list-group scrollList');
+  for(var i = 0; i < data.length; i++){
+    var liCompany = document.createElement('li');
+    liCompany.classList = 'list-group-item';
+    liCompany.textContent = data[i].name;
+    liCompany.data = i;
+    $(ulCompany).append(liCompany);
+  }
+  $('#ListOfCompanies > img').remove();
+  $('#ListOfCompanies').on('click',function(ev){
+    for(var i = 0; i < $('.scrollList').children().length; i++){
+      $('.scrollList').children()[i].classList.remove('chosenCompany');
+    }
+    ev.target.classList += (' chosenCompany');
+    console.log(data[ev.target.data]);
+  });
+  $('#ListOfCompanies').append(ulCompany);
+}
+
+
+function companiesByLocation(mathLocation) {
+  var graphdef = {
+    categories: ['uvCharts'],
+    dataset:{
+      'uvCharts' : mathLocation
+    }
+  };
+  var config = {
+    legend: {
+      position: 'right'
+    },
+    dimension: {
+      width: 180,
+      height: 170
+    },
+    margin: {
+      bottom: 10,
+      top: 15,
+      left: 75
+    },
+    meta: {
+      position: '#CompaniesByLocation'
+    }
+  }
+  $('#CompaniesByLocation > img').remove();
+  var obj = uv.chart('Pie',graphdef, config)
+}
+
+function mathLocation(data) {
+  var objAllCountries = [];
+  for(var i = 0; i < data.length; i++){
+    objAllCountries.push(data[i].location.name);
+  }
+  var objCountries = objAllCountries.slice();
+
+  for(var i = 0; i < objCountries.length;i++){
+    for(var j = i+1; j < objCountries.length;j++){
+      if(objAllCountries[i] == objAllCountries[j]){
+        objCountries.splice(j,1);
+        j--;
+      }
+    }
+  }
+
+  var objMathDrow = [];
+  for( i=0; i < objCountries.length; i++){
+    objMathDrow.push({name: objCountries[i],value: 0});
+    for( j = 0; j < objAllCountries.length; j++){
+      if(objMathDrow[i].name == objAllCountries[j]){
+        objMathDrow[i].value +=1;
+      }
+    }
+  }
+
+  return companiesByLocation(objMathDrow);
 }
